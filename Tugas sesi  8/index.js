b65ab44a95bc1c6baa2bffcd210979861422b5b7
Fuady Anwar Sufijanto <fuadydy@gmail.com>
2022-02-11 20:37:34 +0700

    tugas sesi 8

---
+++ Tugas sesi  8/index.js
@@ -0,0 +1,21 @@
+// ---- Tipe Data JS ------
+// number
+// string
+// undefined
+// null
+// boolean
+// symbol
+// object
+
+var warna = "merah"
+const phi = 3.14
+var hasil = 20 * 22
+var check = 10
+
+
+console.log(warna)
+console.log(phi)
+console.log(hasil)
+console.log(check < 0)
+console.log(check > 0)
+
